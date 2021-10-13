2e517a245dc071e724a60fb38a461c65c53a958f
Jordan Yee <jordanyee799@gmail.com>
2021-10-13 16:31:19 -0700

    commit tree fffa5df6f8ce532548b58c3b42666cdc2c536d2d

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-
+function superbowlWin(objArray) {
+    const holdResult = () => objArray.find(obj => {
+        // console.log(obj);
+        if (obj.result === "W") {
+            return true;
+        } else {
+            return undefined;
+        }
+    })
+    const wDoesExist = holdResult();
+    if (wDoesExist) {
+        return wDoesExist.year
+    } else {
+        return undefined;
+    }
+}
